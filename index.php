e50ef62b36654741def25d8714612604c404362b
torone89 <torone89>
2022-08-29 20:14:19 +0200

    Stampo la variabile text in pagina

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 
+<?php
+$text = "Mark: I wanna hear everyone scream
+fuck you Tom, we fucking hate you,
+you gunna burn in hell and die a horrible firery death
+cause we think you're a stupid piece of shit!"
 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +16,12 @@
     <title>Document</title>
 </head>
 <body>
+
+<!-- stampo la variabile text -->
+<p>
+    <?php  echo $text; ?>
+  </p>
+
     
 </body>
 </html>
